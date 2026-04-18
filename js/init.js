7b86a80976d339d48f715d64732697b730afc50c
Sanidhya Pratap Singh <sanidhya235@gmail.com>
2026-04-18 19:28:43 +0100

    redesign

--- js/init.js
+++ js/init.js
@@ -10,11 +10,6 @@
 /* FitText Settings
 ------------------------------------------------------ */
 
-    setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	 }, 100);
-
-
 /*----------------------------------------------------*/
 /* Smooth Scrolling
 ------------------------------------------------------ */
@@ -78,23 +73,12 @@
 ------------------------------------------------------*/
 
    $(window).on('scroll', function() {
-
-		var h = $('header').height();
-		var y = $(window).scrollTop();
       var nav = $('#nav-wrap');
-
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
-	   }
-      else {
-         if (y < h*.20) {
-            nav.removeClass('opaque').fadeIn('fast');
-         }
-         else {
-            nav.addClass('opaque').fadeIn('fast');
-         }
+      if ($(window).scrollTop() > 8) {
+         nav.addClass('opaque');
+      } else {
+         nav.removeClass('opaque');
       }
-
 	});
 
 
